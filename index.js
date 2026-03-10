2d828d2f2fbfac6fe8b47d8f800ca8d86729c320
Irshad-Mehsud <irshadahmad19059@gmail.com>
2026-03-10 11:59:39 +0500

    Adding API for saving the AI Analyzed Report data

--- index.js
+++ index.js
@@ -1,39 +1,74 @@
-import Express from "express";
+// import Express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import router from "./routes/index.js";
+// import connectDB from "./src/config/db.js";
+// dotenv.config();
+
+
+
+
+
+// const app = Express();
+// app.use(Express.json());
+// app.use(Express.urlencoded({ extended: true }));
+
+// const PORT = process.env.PORT || 5000;
+
+
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+// }));
+
+// connectDB();
+
+// app.use("/api", router);
+
+// app.get("/", (req,res)=>{
+//     res.send("Hello World");
+
+// });
+
+
+// app.listen(PORT, ()=>{
+//     console.log(`Server is running on port ${PORT}`);
+
+// });
+
+
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import router from "./routes/index.js";
-import connectDB from "./src/config/db.js";
+import router from "../routes/index.js";
+import connectDB from "../src/config/db.js";
+
 dotenv.config();
 
+const app = express();
 
-
-
-
-const app = Express();
-app.use(Express.json());
-app.use(Express.urlencoded({ extended: true }));
-
-const PORT = process.env.PORT || 5000;
-
-
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
-
+// Connect database
 connectDB();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://healthmate-frontend-sepia.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+// Routes
 app.use("/api", router);
 
-app.get("/", (req,res)=>{
-    res.send("Hello World");
-
+app.get("/", (req, res) => {
+  res.send("Bootcamp Tracker API Running 🚀");
 });
 
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-
-});
-
-
+export default app;
