fa8777ea8e8fc8d98abf69151793fc07a4b672f3
Irshad-Mehsud <irshadahmad19059@gmail.com>
2026-03-10 12:35:34 +0500

    Adding API for saving the AI Analyzed Report data

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(
   cors({
     origin: [
       "https://healthmate-frontend-sepia.vercel.app",
-      "http://localhost:3000"
+      "http://localhost:3000",
+      "http://localhost:5173"
     ],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
